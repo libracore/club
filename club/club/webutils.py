f87655090b963b6879c7c9df1dca5816d46634eb
lasalesi <larsericmueller@gmail.com>
2018-12-18 16:26:34 +0100

    event interface

--- club/club/webutils.py
+++ club/club/webutils.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018, libracore (https://www.libracore.com) and contributors
 # For license information, please see license.txt
+import frappe
 
 @frappe.whitelist(allow_guest=True)
-def get_next_date():
+def get_next_date(event_type="Spieltag"):
     sql_query = """SELECT
-                  `starts_on`
+                  `subject`, `starts_on`, `ends_on`
                 FROM `tabEvent`
                 WHERE
-                  `event_type` = "Spieltag"
-                  AND `starts_on` > CURDATE()
+                  `event_type` = "{event_type}"
+                  AND `starts_on` >= CURDATE()
                 ORDER BY `starts_on` ASC
-                LIMIT 1;"""
+                LIMIT 1;""".format(event_type=event_type)
     next_date = frappe.db.sql(sql_query, as_dict=True)
     return next_date
